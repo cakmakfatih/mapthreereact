f121378a0d199434822e47449edf451837bb5976
Fatih Çakmak <cakmakfatih@outlook.com.tr>
2019-02-11 15:01:16 +0300

    lines are added but commented out

--- src/lib/builder/Builder.js
+++ src/lib/builder/Builder.js
@@ -34,31 +34,51 @@ export default class Builder {
                 'urlTemplate' : 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
                 'subdomains'  : ['a','b','c','d']
             }),
+            /*
             layers: [
-                new maptalks.VectorLayer('Venue'),
-                new maptalks.VectorLayer('Buildings')
+                new maptalks.VectorLayer('line', {
+                    enableAltitude: true,
+                    forceRenderOnMoving: true,
+                    forceRenderOnRotating : true
+                })
             ]
+            */
         });
 
-        var threeLayer = new ThreeLayer('t', {
+        let threeLayer = new ThreeLayer('t', {
             forceRenderOnMoving : true,
             forceRenderOnRotating : true
         });
 
         let t = this;
         Levels.features.forEach((i: any) => this.levels[i.properties.LEVEL_ID] = i);
+        /* line
+        Units.features.forEach((f: any) => {
+            if(t.levels[f.properties.LEVEL_ID].properties.ORDINAL === t.activeLevel) {
+                f.geometry.coordinates.forEach((j: any) => {
+                    let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
 
+                    var l = new maptalks.LineString(maptalks.GeoJSON.toGeometry(g)._coordinates, {
+                        properties: {
+                            'altitude': 2
+                        }
+                    }).addTo(this.map.getLayer('line'));
+                });
+            }
+        });
+        */
+        
         threeLayer.prepareToDraw = function (gl, scene, camera) {
-            var light = new THREE.DirectionalLight(0xffffff, 1.5);
+            let light = new THREE.DirectionalLight(0xffffff, 1.5);
             light.position.set(0, 7, 7).normalize();
             scene.add(light);
 
             Venue.features.forEach((f: any) => {
                 f.geometry.coordinates.forEach((j: any) => {
-                    let color = 0x66bb6a;
-                    var m = new THREE.MeshPhongMaterial({color: color});
+                    let color = 0xffffff;
+                    let m = new THREE.MeshPhongMaterial({color: color});
                     let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
-                    var mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), 1, m);
+                    let mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), 1, m);
 
                     if (Array.isArray(mesh)) {
                         scene.add.apply(scene, mesh);
@@ -67,14 +87,16 @@ export default class Builder {
                     }
                 });
             });
-            /*
+            
+            
+
             Buildings.features.forEach((f: any) => {
                 f.geometry.coordinates.forEach((j: any) => {
                     let color = 0x607d8b;
-                    var m = new THREE.MeshPhongMaterial({color: color});
+                    let m = new THREE.MeshPhongMaterial({color: color});
                     let sM = new THREE.MeshPhongMaterial({ color: 0x78909c });
                     let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
-                    var mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), f.properties.HEIGHT + 1, [m, sM]);
+                    let mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), f.properties.HEIGHT + 2, [m, sM]);
 
                     if (Array.isArray(mesh)) {
                         scene.add.apply(scene, mesh);
@@ -83,56 +105,61 @@ export default class Builder {
                     }
                 });
             });
-            */
+
+            
+            
             Units.features.forEach((f: any) => {
-                f.geometry.coordinates.forEach((j: any) => {
+                if(t.levels[f.properties.LEVEL_ID].properties.ORDINAL === t.activeLevel) {
+                    f.geometry.coordinates.forEach((j: any) => {
+                        let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
                     
-                    
-                    let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
-                    
-                    if(typeof t.unitColors[f.properties.CATEGORY] === "undefined") {
+                        if(typeof t.unitColors[f.properties.CATEGORY] === "undefined") {
+                            let red = t.randomColor();
+                            let green = t.randomColor();
+                            let blue = t.randomColor();
+
+                            t.unitColors[f.properties.CATEGORY] = parseInt(red + green + blue, 16);
+                        }
+                        
+                        let m = new THREE.MeshPhongMaterial({color: t.unitColors[f.properties.CATEGORY]});
+                        let geo = maptalks.GeoJSON.toGeometry(g);
+                        let mesh = this.toExtrudeMesh(geo, 2, m);
+
+                        if (Array.isArray(mesh)) {
+                            scene.add.apply(scene, mesh);
+                        } else {
+                            scene.add(mesh);
+                        }
+                    });
+                }
+            });
+
+            Fixtures.features.forEach((f: any) => {
+                if(t.levels[f.properties.LEVEL_ID].properties.ORDINAL === t.activeLevel) {
+                    f.geometry.coordinates.forEach((j: any) => {
                         let red = t.randomColor();
                         let green = t.randomColor();
                         let blue = t.randomColor();
 
-                        t.unitColors[f.properties.CATEGORY] = parseInt(red + green + blue, 16);
-                    }
-                    
-                    let m = new THREE.MeshPhongMaterial({color: t.unitColors[f.properties.CATEGORY]});
-                    var mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), 2, m);
+                        let color = parseInt(red + green + blue, 16);
+                        let m = new THREE.MeshPhongMaterial({color: color});
+                        let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
+                        let mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), 5, m);
 
-                    if(t.levels[f.properties.LEVEL_ID].properties.ORDINAL === t.activeLevel) {
                         if (Array.isArray(mesh)) {
                             scene.add.apply(scene, mesh);
                         } else {
                             scene.add(mesh);
                         }
-                    }
-                });
-            });
-
-            Fixtures.features.forEach((f: any) => {
-                f.geometry.coordinates.forEach((j: any) => {
-                    let red = t.randomColor();
-                    let green = t.randomColor();
-                    let blue = t.randomColor();
-                    let color = parseInt(red + green + blue, 16);
-                    var m = new THREE.MeshPhongMaterial({color: color});
-                    let g = {...f, geometry: {...f.geometry, type: "Polygon", coordinates: j}, properties: {...f.properties, type: "Polygon"}};
-                    var mesh = this.toExtrudeMesh(maptalks.GeoJSON.toGeometry(g), 5, m);
-
-                    if(t.levels[f.properties.LEVEL_ID].properties.ORDINAL === t.activeLevel) {
-                        if (Array.isArray(mesh)) {
-                            scene.add.apply(scene, mesh);
-                        } else {
-                            scene.add(mesh);
-                        }
-                    }
-                });
+                    });
+                }
             });
         }
 
+        
+
         this.map.addLayer(threeLayer);
+        //this.map.getLayer('line').bringToFront();
     }
 
     randomColor = (): string => {
